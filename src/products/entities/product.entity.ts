4c593c7d8a05bac28023797b53fd99c016ec1e60
Alexis <acruz113@alumnos.uaq.mx>
2026-05-16 21:23:06 -0600

    Better get methods

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,6 @@
 import { Provider } from "src/providers/entities/provider.entity";
 import { Entity, Column, PrimaryGeneratedColumn, IsNull, ManyToOne } from "typeorm";
+
 @Entity()
 export class Product {
     @PrimaryGeneratedColumn("uuid")
@@ -10,8 +11,8 @@ export class Product {
     price: number;
     @Column({type: "int"})
     countSeal: number;
-    // @Column({type: "uuid"})
-    //provider: string;
-    @ManyToOne(() => Provider, (provider) => provider.products)
+    @ManyToOne(() => Provider, (provider) => provider.products,{
+        eager: true
+})
     provider: Provider
 }
--- src/providers/providers.service.ts
+++ src/providers/providers.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Like } from 'typeorm';
 import { Provider } from './entities/provider.entity';
 
 @Injectable()
@@ -18,18 +18,38 @@ export class ProvidersService {
   }
 
   findAll() {
-    return `This action returns all providers`;
+    return this.providerRepository.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} provider`;
+  findOne(id: string) {
+    return this.providerRepository.findOneBy({
+      providerId: id
+    });
   }
 
-  update(id: number, updateProviderDto: UpdateProviderDto) {
-    return `This action updates a #${id} provider`;
+  async findOneByName(name: string) {
+    const provider = await this.providerRepository.findBy({
+      providerName: Like(`%${name}%`)
+    })
+    if (!provider) throw new NotFoundException()
+    return provider
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} provider`;
+ async update(id: string, updateProviderDto: UpdateProviderDto) {
+    const providerToUpdate = await this.providerRepository.preload({
+      providerId: id,
+      ...updateProviderDto
+    })
+    if (!providerToUpdate){
+       throw new NotFoundException()
+    }
+    this.providerRepository.save(providerToUpdate)
+    return providerToUpdate
+  }
+
+  remove(id: string) {
+    this.providerRepository.delete({
+      providerId: id
+    })
   }
 }
